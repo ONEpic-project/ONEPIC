fc8acca9087118debaba27057c28410c07c817a4
danbihyun <dbhyun02@gmail.com>
2025-12-10 17:07:59 +0900

    main.py 주석 추가

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,6 +11,12 @@ app.include_router(products.router, prefix="/api/products")
 def root():
     return {"message": "ONEPIC Backend Running!"}
 
+
+
+
+# 가상환경 접속
+# source fastapi_env/bin/activate
+
 # 실행 명령어
 # uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
 
