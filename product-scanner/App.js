c9f662f1cfd26401be5a205f2827cb450b5de3df
wnstj999 <wnstj99999@gmail.com>
2025-12-15 16:21:20 +0900

    expo go 에러 수정

--- product-scanner/App.js
+++ product-scanner/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import ScanScreen from './product-scanner/screens/ScanScreen';
+import ScanScreen from './screens/ScanScreen';
 
 const Stack = createStackNavigator();
 
